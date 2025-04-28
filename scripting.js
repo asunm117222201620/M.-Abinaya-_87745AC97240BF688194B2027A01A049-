67b1279740b5050a2b620ad83aae35e50e39ef76
Abinaya <144198471+asunm117222201620@users.noreply.github.com>
2025-04-28 20:28:49 +0530

    Delete scripting.js

--- scripting.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const sidebar = document.querySelector(".sidebar");
-
-    menuToggle.addEventListener("click", () => {
-        sidebar.classList.toggle("expanded");
-    });
-});
